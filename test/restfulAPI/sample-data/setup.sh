1ed61dec8e6bb1f2b49b68985b8bdf16c2ee99df
jooho <jlee@redhat.com>
2022-08-26 23:29:01 -0400

    update sample data

--- test/restfulAPI/sample-data/setup.sh
+++ test/restfulAPI/sample-data/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Test environment
-oc new-project test-if
+oc new-project if-templates
 oc create -f ./ovms-template.yaml
 oc create -f ./storage-s3-template.yaml
 oc create -f ./odhintegration-crd.yaml
+oc create -f ./odhintegration-cr-openvino.yaml
 
 # create sample data
-oc create -f ./odhintegration-cr-openvino.yaml
+oc new-project test-if
 oc create -f ./s3-test-secret.yaml
